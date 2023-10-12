28a59cc900c7208177a10b1026ba96402ed061b8
Douglas Viegas <54741486+douglasdmv@users.noreply.github.com>
2023-10-11 22:58:24 -0300

    fix: aws ses config

--- src/main/java/com/br/emailservice/infra/ses/AwsSesConfig.java
+++ src/main/java/com/br/emailservice/infra/ses/AwsSesConfig.java
@@ -1,8 +1,11 @@
 package com.br.emailservice.infra.ses;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceAsyncClientBuilder;
@@ -10,9 +13,15 @@ import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceAsyncClientBui
 @Configuration
 public class AwsSesConfig {
 	
+	@Value("${accessKeyId}")
+	String accessKey;
+	
+	@Value("${secretKey}")
+	String secretKey;
+	
 	@Bean
 	public AmazonSimpleEmailService amazonSimpleEmailService() {
-		return AmazonSimpleEmailServiceAsyncClientBuilder.standard().withRegion(Regions.US_EAST_2).build();
+		return AmazonSimpleEmailServiceAsyncClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey))).withRegion(Regions.US_EAST_2).build();
 	}
 	
 }
